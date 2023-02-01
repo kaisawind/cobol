70640f97b87dfc655a589857090adddd37b7ad1d
kaisawind <wind.kaisa@gmail.com>
2023-02-01 17:23:10 +0800

    add data

---
+++ asg/conv/data_description_entry.go
@@ -0,0 +1,10 @@
+package conv
+
+import (
+	"github.com/kaisawind/cobol/gen/cobol85"
+	"github.com/kaisawind/cobol/pb"
+)
+
+func DataDescriptionEntry(in cobol85.IDataDescriptionEntryContext) (out *pb.DataDescriptionEntry) {
+	return
+}
